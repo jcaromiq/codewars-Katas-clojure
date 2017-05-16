267616299d9ca66b804a6bdb123e100642ded452
Joaquín Caro <me@joaquin-caro.es>
2017-05-16 23:21:00 +0200

    midje dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.3"]]
   :main ^:skip-aot codewars-katas-clojure.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:plugins [[lein-midje "3.1.3"]]
+                   :dependencies [[midje "1.6.3"]]}}
+  )
