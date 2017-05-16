80bc1fe8c97e4a37272b4d7b792ab3feb1fded32
Joaquín Caro <me@joaquin-caro.es>
2017-05-17 00:05:43 +0200

    Even odd kata with different approaches

--- test/codewars_katas_clojure/even_odd_test.clj
+++ test/codewars_katas_clojure/even_odd_test.clj
@@ -2,9 +2,23 @@
     (:require [clojure.test :refer :all]
               [codewars-katas-clojure.even-odd-kata :refer :all]))
 
-(deftest Examples...
-         (is (= (even-or-odd 2) "Even") )
-         (is (= (even-or-odd 0) "Even") )
-         (is (= (even-or-odd 7) "Odd") )
-         (is (= (even-or-odd 1) "Odd") )
-         )
+(deftest Even-or-odd
+  (is (= (even-or-odd 2) "Even") )
+  (is (= (even-or-odd 0) "Even") )
+  (is (= (even-or-odd 7) "Odd") )
+  (is (= (even-or-odd 1) "Odd") )
+  )
+
+(deftest Even-or-odd-with-ifs
+  (is (= (even-or-odd-with-if 2) "Even") )
+  (is (= (even-or-odd-with-if 0) "Even") )
+  (is (= (even-or-odd-with-if 7) "Odd") )
+  (is (= (even-or-odd-with-if 1) "Odd") )
+  )
+
+(deftest Even-or-odd-with-multi-method
+  (is (= (even-or-odd-with-multi 2) "Even") )
+  (is (= (even-or-odd-with-multi 0) "Even") )
+  (is (= (even-or-odd-with-multi 7) "Odd") )
+  (is (= (even-or-odd-with-multi 1) "Odd") )
+  )
