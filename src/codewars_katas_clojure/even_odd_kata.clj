23855d2c55761a8490cd73ab84b7920e69ced0ca
Joaquín Caro <me@joaquin-caro.es>
2017-05-17 00:05:24 +0200

    Even odd kata with different approaches

--- src/codewars_katas_clojure/even_odd_kata.clj
+++ src/codewars_katas_clojure/even_odd_kata.clj
@@ -1,6 +1,17 @@
  (ns codewars-katas-clojure.even-odd-kata)
- (defn even-or-odd [number]
+
+(defn even-or-odd [number]
+  (if (even? number)
+    "Even"
+    "Odd")
+  )
+
+(defn even-or-odd-with-if [number]
    (if (= (mod number 2) 0)
      "Even"
      "Odd")
    )
+
+(defmulti even-or-odd-with-multi (fn [number] (even? number)))
+(defmethod even-or-odd-with-multi true [_] "Even")
+(defmethod even-or-odd-with-multi false [_] "Odd")
