c0162f93ac8e60ca950043cd3a30ea5359289b96
Joaquín Caro <me@joaquin-caro.es>
2017-05-17 22:41:47 +0200

    cloverage plugin

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]]
-  :main ^:skip-aot codewars-katas-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[lein-midje "3.1.3"]]
+             :dev {:plugins [[lein-midje "3.1.3"] [lein-cloverage "1.0.9"]]
                    :dependencies [[midje "1.6.3"]]}}
   )
